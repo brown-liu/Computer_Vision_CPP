6e2c9ca0b4f86aa24e9801740f514ca194ebfbae
BROWN <63453889+brownliuinnz@users.noreply.github.com>
2020-10-05 21:29:49 +1300

    update

--- main.cpp
+++ main.cpp
@@ -2,12 +2,43 @@
 #include <opencv2/opencv.hpp>
 
 using namespace std;
+
+
+#include <opencv2/opencv.hpp>
+
 using namespace cv;
+int main(int argc, char** argv)
+{
+VideoCapture cap;
+if (argc > 1)
+cap.open(argv[1]);
+else
+cap.open(0);
 
-int main(int, char**) {
+if (!cap.isOpened())
+{
+	std::cerr << "Cannot read video. Try moving video file to sample directory." << std::endl;
+	return -1;
+}
 
-    string image_path="/Users/liubo/vscodeProjects/cmakequickstart/images/dog.jpg";
-    Mat image = imread(image_path,IMREAD_COLOR);
-    imshow("img",image);
-    waitKey();
+namedWindow("FG Segmentation", WINDOW_NORMAL);
+
+Mat frame,segm;
+for (;;)
+{
+	cap >> frame;
+	if (frame.empty())
+		break;
+	frame.copyTo(segm);
+
+	imshow("FG Segmentation", segm);
+
+
+	int c = waitKey(30);
+	if (c == 'q' || c == 'Q' || (c & 255) == 27)
+		break;
+}
+
+
+return 0;
 }
